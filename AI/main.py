04980cf53077768954b45218cdb32f433bb9ad4b
row-huh <RohaAbroPathan@gmail.com>
2024-05-20 15:57:22 +0500

    changed model

--- AI/main.py
+++ AI/main.py
@@ -6,7 +6,7 @@ project_id = "afterschool125"
 
 vertexai.init(project=project_id, location="us-central1")
 
-model = GenerativeModel(model_name="gemini-1.5-pro")
+model = GenerativeModel(model_name="gemini-1.0-pro-002")
 
 response = model.generate_content(
     "What's a good name for a flower shop that specializes in selling bouquets of dried flowers?"
