ba1e3d17ca9988f8776d0cc6cc4e5bc758fa5b10
row-huh <RohaAbroPathan@gmail.com>
2024-05-20 15:50:31 +0500

    added agent.html for testing

--- AI/main.py
+++ AI/main.py
@@ -1,1 +1,15 @@
-from google.cloud import aiplatform
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
+# TODO(developer): Update and un-comment below line
+project_id = "afterschool125"
+
+vertexai.init(project=project_id, location="us-central1")
+
+model = GenerativeModel(model_name="gemini-1.5-pro")
+
+response = model.generate_content(
+    "What's a good name for a flower shop that specializes in selling bouquets of dried flowers?"
+)
+
+print(response.text)
--- backend/app.py
+++ backend/app.py
@@ -12,4 +12,4 @@ def submit_form():
     return "Form data received successfully"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
